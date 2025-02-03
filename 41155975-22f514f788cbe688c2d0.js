74635788b5e2e9a5422d5d777060f9bc1e48ddcc
Divyangi Mahajan <divyangi.mahajan17@gmail.com>
2025-02-03 19:38:14 +0530

    Updates

---
+++ 41155975-22f514f788cbe688c2d0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdivyangi_portfolio=self.webpackChunkdivyangi_portfolio||[]).push([[396],{576:function(t,i,n){n.d(i,{LCd:function(){return o}});var a=n(9609);function o(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=41155975-22f514f788cbe688c2d0.js.map
