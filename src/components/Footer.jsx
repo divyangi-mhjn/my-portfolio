caaaa6da86ce8fea65486dbcc0f3beb8e1b322e1
Divyangi Mahajan <divyangi.mahajan17@gmail.com>
2025-05-06 13:13:31 +0530

    May Month Update 2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <span className="taglineFooter">Code. Create. Innovate.</span>
-        <p className="copyright">&copy;Divyangi Mahajan.</p>
+        <span className="taglineFooter">Solving Problems, One code at a time</span>
+        <p className="copyright">&copy;Divyangi Mahajan, 2025</p>
     </footer>
 )
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import downloadFile from '../assets/document/divyangi-mahajan.pdf'
 import { FaGithub, FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
+import { BsMedium } from 'react-icons/bs';
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
@@ -10,19 +11,22 @@ const Header = props => (
       <div className="inner">
         <h1>Divyangi Mahajan</h1>
         <p>
-          Elevating user Experiences
+          Coding The Future
         </p>
         <ul className="icons">
-          <li>
+          <li title = {'linkedin.com/in/divyangi-mahajan'} >
             <a href="https://www.linkedin.com/in/divyangi-mahajan"><FaLinkedinIn size={17} style={{ verticalAlign: 'sub' }} aria-label="Linkedin"/></a>
           </li>
-          <li>
+          <li title={'github.com/divyangi-mhjn'}>
             <a href="https://www.github.com/divyangi-mhjn" ><FaGithub size={17} style={{ verticalAlign: 'sub' }} aria-label="github" /></a>
           </li>
-          <li>
+          <li title= {'medium.com/@MahajanInsights'}>
+            <a href="https://medium.com/@MahajanInsights" ><BsMedium size={17} style={{ verticalAlign: 'sub' }} aria-label="github" /></a>
+          </li>
+          <li title={'x.com/MahajanInsights'}>
             <a href="https://x.com/MahajanInsights"> <FaXTwitter size={17} style={{ verticalAlign: 'sub' }} /></a>
           </li>
-          <li>
+          <li title={'facebook.com/divyangi.mahajan'}>
             <a href="https://www.facebook.com/divyangi.mahajan"> <FaFacebookF size={17} style={{ verticalAlign: 'sub' }} /></a>
           </li>
         </ul>
